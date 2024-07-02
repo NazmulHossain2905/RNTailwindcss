bf0b7c2423923570ae37b556f749d09585f379e1
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2024-07-02 19:45:17 +0600

    modified Home.tsx file and create a new `nativewind-env.d.ts` file to resolve className error

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -3,8 +3,8 @@ import {Text, View} from 'react-native';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View className="justify-center items-center bg-white dark:bg-black flex-1">
+      <Text className="text-3xl text-black dark:text-white">Hello</Text>
     </View>
   );
 };
